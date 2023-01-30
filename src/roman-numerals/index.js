d9b5b4d334594efe497629bfbb3e4eb64e962322
shittu wasiu <abdulwasiushittu1416@gmail.com>
2023-01-30 15:24:32 +0100

    Changed

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,32 +4,54 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {
-  var romanNumeral = {
-    M: 1000,
-    CM: 900,
-    D: 500,
-    CD: 400,
-    C: 100,
-    XC: 90,
-    L: 50,
-    XL: 40,
-    X: 10,
-    IX: 9,
-    V: 5,
-    IV: 4,
-    I: 1
-  };
-  var str = '';
-
-  for (var index of Object.keys(romanNumeral)) {
-    var getVal = Math.floor(roman / romanNumeral[index]);
-    roman -= getVal * romanNumeral[index];
-    str += index.repeat(getVal);
-  }
-
-  return str;
-
+function value(roman) 
+{
+    if (roman == 'I')
+        return 1;
+    if (roman == 'V')
+        return 5;
+    if (roman == 'X')
+        return 10;
+    if (roman == 'L')
+        return 50;
+    if (roman == 'C')
+        return 100;
+    if (roman == 'D')
+        return 500;
+    if (roman == 'M')
+        return 1000;
+    return -1;
+}
+function romanToDecimal(roman) 
+{
+  
+    var result = 0;
+  
+     for (i = 0; i < roman.length; i++) 
+     {
+         var s1 = value(roman.charAt(i));
+         
+         if (i + 1 < roman.length) 
+         {
+             var s2 = value(roman.charAt(i + 1));
+  
+             // Comparing both values
+             if (s1 >= s2) 
+             {
+                 result = result + s1;
+             } 
+             else 
+             {
+              result = result + s2 - s1;
+              i++;
+             }
+         } 
+         else  
+         {
+          result = result + s1;
+         }
+     }
+     return result;
 }
 
 module.exports = romanToDecimal;
